6657d878dcaf7f693018a97130d8aa1f7770959a
maggie <meghanapullagura2606@gmail.com>
2020-06-23 17:12:22 +0530

    Updates

---
+++ precache-manifest.329baaf40aabedb5b5c698d90b9e37e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e92cf7abc64cb2263e6cc74940fb196",
+    "url": "/myproject1/index.html"
+  },
+  {
+    "revision": "b366ccf3a79800435b58",
+    "url": "/myproject1/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "637b4512fda2f25d32ed",
+    "url": "/myproject1/static/js/2.cfa8ccda.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myproject1/static/js/2.cfa8ccda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b366ccf3a79800435b58",
+    "url": "/myproject1/static/js/main.e83c2816.chunk.js"
+  },
+  {
+    "revision": "962714ba67cb697d91e6",
+    "url": "/myproject1/static/js/runtime-main.1125f0d7.js"
+  }
+]);
